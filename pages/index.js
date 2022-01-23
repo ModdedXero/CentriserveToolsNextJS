887511b0574ccd69866b9ea1d59ab72242b6fc2e
Xero Gaming <xerosgame@hotmail.com>
2022-01-23 02:33:44 -0700

    Init project with NextJS and Bootstrap

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Navbar } from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <div className="page-main">
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Container>
+
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
